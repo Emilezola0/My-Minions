f447df8a68be5504a86924215884112e14fa4a51
Emilezola0 <emilelarguier2@gmail.com>
2024-01-30 15:33:29 +0100

    probleme with spawning inside

--- Assets/Script/CircleController.cs
+++ Assets/Script/CircleController.cs
@@ -4,27 +4,29 @@ using UnityEngine;
 
 public class CircleController : MonoBehaviour
 {
-    public float speedGrowth = 1f;
+    public float circleScale;
     private LineRenderer lineRenderer;
 
     // Ref Of Territory
-    public GameObject transformTerritory;
+    public GameObject territory;
 
     // Start is called before the first frame update
     void Start()
     {
         // Need Line Renderer Attached
         lineRenderer = GetComponent<LineRenderer>();
+
+        // Circle Growth modifying the width of the Line Renderer
+        lineRenderer.startWidth = lineRenderer.endWidth = circleScale;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Circle Growth modifying the width of the Line Renderer
-        float nouvelleLargeur = lineRenderer.startWidth + (speedGrowth * Time.deltaTime);
-        lineRenderer.startWidth = lineRenderer.endWidth = nouvelleLargeur;
-
         // update territoryTransform
-        transform.position = transformTerritory.transform.position;
+        Vector3 newPosition_ = territory.transform.position;
+        newPosition_.z = 0;
+        lineRenderer.SetPosition(0, newPosition_);
+        lineRenderer.SetPosition(1, newPosition_);
     }
 }
--- Assets/Script/Territory.cs
+++ Assets/Script/Territory.cs
@@ -7,11 +7,20 @@ public class Territory : MonoBehaviour
     [Header ("<b>Player Parameter</b>")]
     [Space (4)]
     public float vitesseDeplacement;
+    public float recoveryTime = 1.0f;
 
     [Space(10)]
     [Header("<b>Invoke</b>")]
     [Space(4)]
     public GameObject minions;
+    public float spawnInterval = 5.0f;
+
+    [Space(10)]
+    [Header("<b>Game Object</b>")]
+    [Space(4)]
+    public GameObject territoryCircle;
+    [Tooltip("This is the inner radius, Game Object will not spawn inside the center of the spawnRadius")]
+    public float innerRadius = 1.0f;
 
     [Space(10)]
     [Header("<b>Sound</b>")]
@@ -20,24 +29,95 @@ public class Territory : MonoBehaviour
     public AudioClip territoryDeployement;
     public AudioClip territoryUnlayable;
 
+    // Private Audio
     private AudioSource audioSource;
+    // Line Renderer
+    private float spawnRadius;
+    private LineRenderer lineRenderer;
+    // Spawn & Mouse
+    private bool canSpawn = true;
+    private bool isMouseDragging = false;
+    // When you MouseUp we put a time to make the animation
+    private float timeSinceMouseUp;
+    // Time
+    private float currentTime;
     // Start is called before the first frame update
     void Start()
     {
-          audioSource = GetComponent<AudioSource>();  
+        audioSource = GetComponent<AudioSource>();
+        lineRenderer = territoryCircle.GetComponent<LineRenderer>();
+        
+        // Calculate Spawn Radius
+        CalculateSpawnRadius();
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //
+        if(isMouseDragging == false)
+        {
+            if (canSpawn == true)
+            {
+                // If the invocation time has elapsed
+                if (currentTime <= 0)
+                {
+                    currentTime = spawnInterval;
+                    // Invoke Minions
+                    InstantiateTroop(minions);
+                    
+                }
+                else
+                {
+                    // Decrement invocation time
+                    currentTime -= Time.deltaTime;
+                }
+            }
+            // If you cannot summon, count the time since the last minion.
+            else
+            {
+                // The time since the last minion has elapsed, reset the ability to invoke
+                if (timeSinceMouseUp > recoveryTime)
+                {
+                    canSpawn = true;
+                }
+                else
+                {
+                    timeSinceMouseUp += Time.deltaTime;
+                }
+            }
+        }
     }
     private void OnMouseDown()
     {
         PlaySoundOneShot(takingTerritory);
- 
+
+        // Make it impossible to spawn on mouse drag
+        canSpawn = false;
+
+        // Calculate Spawn Radius
+        CalculateSpawnRadius();
     }
 
     private void OnMouseUp()
     {
         PlaySoundOneShot(territoryDeployement);
+
+        // Activate Spawning
+        if (!isMouseDragging)
+        {
+            canSpawn = true; // Can Spawn Troop
+
+            timeSinceMouseUp = 0f; // Reset time since MouseUp
+        }
+
+        isMouseDragging = false; // Reset isMouseDragging to false after MouseUp
+
     }
     private void OnMouseDrag()
     {
+        isMouseDragging = true;
+
         // Get the Mouse Position
         Vector3 mousePosition_ = Input.mousePosition;
         mousePosition_.z = 0;
@@ -51,12 +131,6 @@ public class Territory : MonoBehaviour
         
         // Debug.Log("Position de la souris : " + worldMousePosition);
     }
-    // Update is called once per frame
-    void Update()
-    {
-     
-    }
-
     void PlaySoundOneShot(AudioClip sound)
     {
         if (audioSource != null)
@@ -72,10 +146,59 @@ public class Territory : MonoBehaviour
             Debug.Log("No AudioClip have been detected");
         }
     }
+    void CalculateSpawnRadius()
+    {
+        // Calculate the spawnRadius depending of the size of the circle
+        float circleRadius = lineRenderer.startWidth / 2f;
+        spawnRadius = circleRadius;
+    }
 
     void InstantiateTroop(GameObject troop)
     {
+        // Get a random point inside the line renderer
+        Vector3 randomPoint_ = GetRandomPointOnLine(lineRenderer, Random.Range(innerRadius, 1f - innerRadius));
+
+        // Ajust Position by adding the inner plage
+        Vector3 spawnPosition_ = randomPoint_ + (Vector3)(Random.insideUnitCircle * spawnRadius);
+
         // We will instantiate Troop each time the slider end getting the range of the territory
-        Instantiate(troop, transform.position, Quaternion.identity);
+        Instantiate(troop, spawnPosition_, Quaternion.identity);
+    }
+
+    Vector3 GetRandomPointOnLine(LineRenderer line, float t)
+    {
+        Vector3 lineStart = line.GetPosition(0);
+        Vector3 lineEnd = line.GetPosition(line.positionCount - 1);
+
+        float lineWidth = line.startWidth;
+        float randomOffset = Random.Range(-lineWidth / 2f, lineWidth / 2f);
+
+        // Adjust t based on inner radius
+        float adjustedT = Mathf.Clamp01(t * (1.0f - 2.0f * innerRadius));
+
+        // Get the random point on the adjusted line
+        Vector3 randomPointOnLine = Vector3.Lerp(lineStart, lineEnd, adjustedT);
+
+        // Rejection sampling to get a point within the circle
+        Vector2 randomPoint;
+        do
+        {
+            float angle = Random.Range(0f, 360f);
+            float radius = innerRadius + randomOffset;
+            randomPoint = new Vector2(Mathf.Cos(angle * Mathf.Deg2Rad), Mathf.Sin(angle * Mathf.Deg2Rad)) * radius;
+        } while (Vector2.Distance(randomPoint, Vector2.zero) > innerRadius);
+
+        return randomPointOnLine + new Vector3(randomPoint.x, randomPoint.y, 0f);
+    }
+
+    private void OnDrawGizmos()
+    {
+        DrawInnerRadiusGizmo();
+    }
+
+    private void DrawInnerRadiusGizmo()
+    {
+        Gizmos.color = Color.red; // You can choose any color
+        Gizmos.DrawWireSphere(transform.position, innerRadius);
     }
 }
