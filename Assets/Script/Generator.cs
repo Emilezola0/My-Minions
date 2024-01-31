7cc479c2c59e578bf60912e32d9ce68b5d02206b
gleufeurg <102815589+gleufeurg@users.noreply.github.com>
2024-01-31 19:07:21 +0100

    Generate map

--- Assets/Script/Generator.cs
+++ Assets/Script/Generator.cs
@@ -1,38 +1,83 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Tilemaps;
-using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class Generator : MonoBehaviour
 {
+    [Header("Map")]
+    [Range(0, 100)] public int sizeX;
+    [Range(0, 100)] public int sizeY;
+
+    [Header("Ressources")]
     [Range(1, 25)]  public int   numberOfRessources;
     [Range(0, 100)] public float ressourceSpawnChances;
     [Range(0, 100)] public float obstacleSpawnChances;
 
     [SerializeField] GameObject[] spawnPoints;
-    public GameObject playerSpawn;
-    public GameObject ressource;
-    public GameObject obstacle;
+    [SerializeField] GameObject playerSpawn;
+    [SerializeField] GameObject ressource;
+    [SerializeField] GameObject obstacle;
+
+    [SerializeField] Tilemap groundTilemap;
+    [SerializeField] Tilemap obstacleTilemap;
+    [SerializeField] Tilemap ressourcesTilemap;
+
+    [SerializeField] Tile groundTile;
+    [SerializeField] Tile obstacleTile;
+    [SerializeField] Tile ressourceTile;
+    [SerializeField] Vector3Int tileLocation;
 
     private void Start()
     {
-        //tilemap = Grid.transform.Find("Ground").GetComponent<Tilemap>();
+        GenerateMap(sizeX, sizeY);
+        SpawnRessources(ressourceSpawnChances);
+        SpawnObstacles(obstacleSpawnChances);
+    }
+
+    private void GenerateMap(int X, int Y)
+    {
+        for (int i = 0; i < X; i++) 
+        {
+            for(int j = 0; j < Y; j++)
+            {
+                tileLocation = new Vector3Int(i, j, 0);
+                groundTilemap.SetTile(tileLocation, groundTile); //Set the tile
+            }
+        }
+    }
+
+    void GetTileAtLocation(Vector3 position)
+    {
+        tileLocation = groundTilemap.WorldToCell(position); //Register the tile position on the tilemap
+    }
+
+    #region Spawns
+
+    void SpawnObstacles(float SpawnChances_)
+    {
+        if (DoesSpawn(SpawnChances_)) //Decides if the obstacle spawns or not
+        {
+            GameObject obstacle_ = GameObject.Instantiate(obstacle/*, .transform*/);
+        }
+    }
+
+    void SpawnRessources(float SpawnChances_)
+    {
 
         for (int i = 0; i < numberOfRessources; i++)
         {
-            if(DoesSpawn(ressourceSpawnChances)) //Decides if the ressource spawns or not
+            if (DoesSpawn(SpawnChances_)) //Decides if the ressource spawns or not
             {
+                tileLocation = groundTilemap.WorldToCell(spawnPoints[i].transform.position);
+
                 GameObject ressource_ = GameObject.Instantiate(ressource, spawnPoints[i].transform);
+                ressource_.transform.position = tileLocation;
 
-                //Obstruct the closest path to go to the ressource with obstacles
-                Physics2D.Raycast(ressource_.transform.position, GetPlayerSpawn());
-                Debug.DrawLine(ressource_.transform.position, GetPlayerSpawn(), Color.red, 60f);
-
-                if (DoesSpawn(obstacleSpawnChances))
-                {
-                    GameObject.Instantiate(obstacle);
-                }
+                //Need to be generated only inside the groundTilemap
             }
         }
     }
@@ -48,5 +93,13 @@ public class Generator : MonoBehaviour
     Vector2 GetPlayerSpawn()
     {
         return playerSpawn.transform.position;
+    }
+
+    #endregion
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(transform.position, new Vector2(sizeX, sizeY));
     }
 }
